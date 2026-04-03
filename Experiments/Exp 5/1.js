c0c4eb6a58d8b01034a4baa6081739ec6abe19e9
soumyasharma3700 <soumyasharma.10@gmail.com>
2026-04-03 23:46:19 +0530

    added

---
+++ Experiments/Exp 5/1.js
@@ -0,0 +1,10 @@
+function addEmployee() {
+
+    let emp = {
+        name: document.getElementById("name").value,
+        id: document.getElementById("id").value,
+        salary: Number(document.getElementById("salary")).value,
+        department: document.getElementById("department").value
+
+    };
+    
