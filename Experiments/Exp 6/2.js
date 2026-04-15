ad899365a6a989d40da66fb43a88e80ce5f31c28
soumyasharma3700 <soumyasharma.10@gmail.com>
2026-04-15 20:43:41 +0530

    update

--- Experiments/Exp 6/2.js
+++ Experiments/Exp 6/2.js
@@ -15,3 +15,21 @@ document.getElementById("fontsizebtn").addEventListener("click", function() {
     heading.style.fontSize = "40px";
 });
 
+let isvsible = true;
+document.getElementById("togglebtn").addEventListener("click", function() {
+    if (isvsible) {
+        para.style.display = "none";
+        isvsible = false;
+
+    } else {
+        para.style.display = "block";
+        isvsible = true;
+    }
+});
+document.getElementById("resetbtn").addEventListener("click", function() {
+    location.reload();
+});
+
+heading.onmouseover = function() {
+    heading.style.color = "red";
+};
